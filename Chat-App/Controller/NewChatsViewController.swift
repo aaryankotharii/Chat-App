c8e01c2258f297ee78ce7c14cdeff4d7440734fd
aaryankotharii <aaryan.kothari@gmail.com>
2020-04-05 00:26:10 +0530

    NewChatsVC tableView setup

--- Chat-App/Controller/NewChatsViewController.swift
+++ Chat-App/Controller/NewChatsViewController.swift
@@ -8,23 +8,28 @@
 
 import UIKit
 
-class NewChatsViewController: UIViewController {
+class NewChatsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
 
+    @IBOutlet weak var newContactsTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        setNavBar()
+    }
+    func setNavBar(){
+        let searchBar = UISearchController(searchResultsController: nil)
+        navigationItem.searchController = searchBar
+        navigationItem.hidesSearchBarWhenScrolling = false
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           return 5
+       }
+       
+       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell =  newContactsTableView.dequeueReusableCell(withIdentifier: "newchatcell") as? NewChatTableViewCell
+       }
+    
+    
 }
---
+++ Chat-App/View/NewChatTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  NewChatTableViewCell.swift
+//  Chat-App
+//
+//  Created by Aaryan Kothari on 05/04/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import UIKit
+
+class NewChatTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var NameLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
