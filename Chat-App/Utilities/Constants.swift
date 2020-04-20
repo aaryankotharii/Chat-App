22cd6f2dcd9b31209eb4483256f7b37ddd5f3e22
aaryankotharii <aaryan.kothari@gmail.com>
2020-04-20 13:17:15 +0530

    ConstantsVC added

---
+++ Chat-App/Utilities/Constants.swift
@@ -0,0 +1,24 @@
+//
+//  Constants.swift
+//  Chat-App
+//
+//  Created by Aaryan Kothari on 20/04/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+//MARK: -  function to get uid
+internal func getUID() -> String {
+    let uid = Auth.auth().currentUser?.uid
+    return uid ?? "notFound"
+}
+
+public func debugLog(message: String) {
+    #if DEBUG
+    debugPrint("=======================================")
+    debugPrint(message)
+    debugPrint("=======================================")
+    #endif
+}
